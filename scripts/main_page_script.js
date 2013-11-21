199e82cc24294c2aa6d85e1b323fd3051a10894e
mhristova <mhristova6@gmail.com>
2013-11-21 13:01:18 +0000

    create the individual page via ajax approach. when you click on a stock on the main page you do not load a new page

--- scripts/main_page_script.js
+++ scripts/main_page_script.js
@@ -38,7 +38,7 @@ function addRow(name, symbol, stocksTable) {
 		var csv = reader.responseText;
 		var data = $.csv.toArrays(csv);
 		var html = '';
-		html += '<tr>\r\n' +
+		html += '<tr onclick=\"onClick(\'' + symbol + '\');\">\r\n' +
 				'<td>' + name + '</td>\r\n' +
 				'<td>' + symbol + '</td>\r\n';
 		var lastDayRow = data[1];
@@ -50,4 +50,61 @@ function addRow(name, symbol, stocksTable) {
 		stocksTable.innerHTML += html;
 	};
 	reader.send();
+}
+
+function onClick(symbol) {
+	var test = document.getElementById('test');
+	test.innerHTML = "HELLO";
+	var stocksTable = document.getElementById("mainContent");
+	stocksTable.className = "HiddenClass";
+	var individualStock = document.getElementById("individualStock");
+	individualStock.className = "";
+	var table = document.getElementById("singleStockTable");
+	initialiseTable(table);
+	var back = document.getElementById("back");
+	back.className = "";
+	var reader = new XMLHttpRequest();
+	reader.open('GET', '../data/' + symbol + '.csv');
+	reader.onreadystatechange = function() {
+		if (reader.readyState != 4) {
+			return;
+		}
+		var csv = reader.responseText;
+		var data = $.csv.toArrays(csv);
+		var html = '';
+		var index = 0;
+		for(var row in data) {
+			if (index > 0) {
+				html += '<tr>\r\n';
+				var i;
+				for (var i = 0; i < 6; i++) {
+					html += '<td>' + data[row][i] + '</td>\r\n';
+				}
+				html += '</tr>\r\n';
+			}
+			index++;
+		}
+		table.innerHTML += html;
+	}
+	reader.send();
+}
+
+function initialiseTable(table) {
+	table.innerHTML = '<tr>\r\n' + 
+				'<th>Trading Day</th>\r\n' + 
+				'<th>Opening Price</th>\r\n' + 
+				'<th>Trading Day\'s High</th>\r\n' + 
+				'<th>Trading Day\'s Low</th>\r\n' + 
+				'<th>Closing Price</th>\r\n' + 
+				'<th>Volume</th>\r\n' +
+				'</tr>';
+}
+
+function goBack() {
+	var back = document.getElementById("back");
+	back.className = "HiddenClass";
+	var individualStock = document.getElementById('individualStock');
+	individualStock.className = "HiddenClass";
+	var mainContent = document.getElementById("mainContent");
+	mainContent.className = "";
 }
